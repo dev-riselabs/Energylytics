208a46a8d600d241058e9d955a021ede0520a5bc
Abosede Oloude Oyamine <oloudeabosede26@gmail.com>
2026-07-30 15:50:49 +0100

    fixed ui

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -41,7 +41,7 @@ function Header() {
             <NavLink
               to={path}
               className={({ isActive }) =>
-                isActive ? "text-green25 text-base" : "text-slate text-base"
+                isActive ? "text-green text-base font-semibold" : "text-slate text-base"
               }
             >
               {name}
@@ -49,10 +49,10 @@ function Header() {
           ))}
         </nav>
         <div className="flex flex-col gap-4">
-          <button className="px-5 py-3 rounded-xl border border-green30 text-green25 text-sm font-bold font-dmSans hover:bg-green30 hover:text-white transition-all cursor-pointer">
+          <button className="px-5 py-3 rounded-xl border border-green30 text-green text-sm font-bold font-dmSans hover:bg-green25 hover:text-white transition-all cursor-pointer">
             Get a demo
           </button>
-          <button className="px-5 py-3 rounded-xl border border-green bg-green25 text-white text-sm font-bold font-dmSans hover:bg-transparent hover:text-green25 transition-all cursor-pointer">
+          <button className="px-5 py-3 rounded-xl border border-green bg-green25 text-white text-sm font-bold font-dmSans hover:bg-transparent hover:text-green transition-all cursor-pointer">
             Login/Sign Up
           </button>
         </div>
@@ -64,7 +64,7 @@ function Header() {
             <NavLink
               to={path}
               className={({ isActive }) =>
-                isActive ? "text-green text-base" : "text-slate text-base"
+                isActive ? "text-green text-base font-semibold" : "text-slate text-base"
               }
             >
               {name}
